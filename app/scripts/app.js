c20e2f82c690844c62b702c65683cb5063d29643
Christine <cylee4@gmail.com>
2015-01-19 18:57:23 -0800

    Add test url using angular-feeds

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,7 @@
 angular
   .module('HReadly', [
     'HReadly.services',
+    'feeds',
     'ngAnimate',
     'ngCookies',
     'ngResource',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,41 @@
  */
 angular.module('HReadly')
   .controller('MainCtrl', function ($scope, $http, Feeds) {
+    $scope.feeds = [{
+      url: 'http://dailyjs.com/atom.xml',
+      items: []
+    }];
     var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fdailyjs.com%2Fatom.xml'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
     Feeds.getFeed(url).success(function(data){
       $scope.feed = data.query.results.entry;
       console.log('$scope.feed in Main:', $scope.feed);
     });
+
+    $scope.addFeed = function(feed) {
+      $scope.feeds.push(feed);
+      $scope.fetchFeed(feed);
+      $scope.newFeed = {};
+    };
+
+    $scope.fetchFeed = function(feed) {
+      console.log('fetchFeed firing');
+      feed.items = [];
+      
+      var apiUrl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'";
+      apiUrl += encodeURIComponent(feed.url);
+      apiUrl += "'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+
+      $http.jsonp(apiUrl)
+      .success(function(data, status, headers, config){
+        console.log('fetching feed successful');
+        console.log(data);
+        if (data.query.results) {
+          feed.items = data.query.results.entry;
+        }
+      })
+      .error(function(data, status, headers, config) {
+        console.error('Error fetching feed:', data);
+      });
+    };
 
   });
